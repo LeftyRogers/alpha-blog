563a21c406dcc497b450e2d079f5e3709b9a5dbd
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-17 13:37:51 -0600

    Add show feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  # :articles provides everything; only restricts to a single route
+  resources :articles, only: [:show]
 end

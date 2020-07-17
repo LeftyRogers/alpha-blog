2f177c374c09438b5c815a9af2f43c943102aada
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-17 13:59:18 -0600

    Add articles index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   # :articles provides everything; only restricts to a single route
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
 end

9fc358c1bbc778592e4f4f24d72cb127c5c385a8
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-20 07:32:27 -0600

    Add navigation links to all pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  # removing 'only' because (a) all methods are exposed and (b) exposes RESTful transfers
+  resources :articles
 end

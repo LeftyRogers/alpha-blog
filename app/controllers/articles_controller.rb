f907f6ad958b53bd6a5dd073e5090aaff410b755
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-20 06:46:14 -0600

    Add edit and update functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,13 +8,36 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    # handled entirely in .html.erb file
+    # Essentially declaring a variable for the .html.erb file
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
+    # params.require(:article) means that 'article' must appear in the parameters list for it to work
+    # permit(:title, :description) means that only title and description will be set upon creation
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
-    redirect_to @article
+
+    if @article.save
+      flash[:notice] = "Article has been saved"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
 end

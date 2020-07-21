4ef57491d8d185804114866fdfb2338646bc9168
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-21 12:06:11 -0600

    More CSS / Bootstrap candy

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Added for something to do with errors, ostensibly to stop showing field with errors?
+# However it's not being shown anyway.
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end

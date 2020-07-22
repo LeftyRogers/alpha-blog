8fd2dffba79acc3479f79d092de5b32da58f9bb1
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-21 18:25:00 -0600

    Add user signup link to home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(strong_parameters)
+
+    if @user.save
+      flash[:notice] = "Congratulations, #{@user.username}, you are now signed up for Alpha Blog!"
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def strong_parameters 
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end

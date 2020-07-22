10599fd355d929ad2dc4b9a48e1331c445e07e99
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-21 18:09:23 -0600

    Add user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  # clarify route to About page
   get 'about', to: 'pages#about'
 
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   # removing 'only' because (a) all methods are exposed and (b) exposes RESTful transfers
   resources :articles
+
+  # redirect route for signup to the new method
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resource :users, except: [:new]
 end

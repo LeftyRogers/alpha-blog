3c6fe48f8eeb40bebe2a497f20e01872d27d2ca5
Mark Rogers <mark.f.rogers.phd@gmail.com>
2020-07-23 07:59:16 -0600

    Add user edit and update capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
+  end
+
+  def edit
+    # already set in before_action
   end
 
   def create
@@ -14,10 +20,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(strong_parameters)
+      flash[:notice] = "#{@user.username}, your record has been updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def strong_parameters 
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
